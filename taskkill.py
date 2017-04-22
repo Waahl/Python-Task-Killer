776b4c5af9dec5a1343c75bbc7a96a9325b031ac
Waahl <mr.jacobwahlman@gmail.com>
2017-04-22 16:03:45 +0200

    Most of the logic added
    Now a working prototype.

--- taskkill.py
+++ taskkill.py
@@ -10,43 +10,55 @@ import sys
 import os
 
 # TODO
-class TaskKill:
 
-    def __init__(self):
+def fetch_data(process):
+    """
+    Fetches the config data from a json file.
+    """
+    with open("config.json", "r+", encoding="utf-8") as config:
+        data = json.load(config)
+        if process in data["processes"]:
+            kill_process(process)
+
+def check_process():
+    """
+    Checks which processes are running and wether to end those processes or not.
+    """
+    data = [x.decode("windows-1252").split(",") for x in \
+    subprocess.check_output("TASKLIST /FO csv").splitlines()][3:]
+
+    proc = [x[0][1:-1] for x in data]
+
+    for process in proc:
+        fetch_data(process)
+
+def kill_process(process):
+    """
+    Calls the TASKKILL command to Windows CMD.
+    """
+    status = []
+    try:
+        proc_status = subprocess.check_output("TASKKILL /F /T /IM {}".format(process)).decode("utf-8")
+        status.append(proc_status)
+    except subprocess.CalledProcessError:
+        print("Failed to find task, skipping.")
+        status.append("ERROR")
         pass
+    log_information(status)
 
-    def fetch_data(self):
-        """
-        Fetches the config data from a json file.
-        """
-        with open("config.json", "r+", encoding="utf-8") as config:
-            data = json.load(config)
-            # Add logic
-            config.seek(0)
-            json.dump(data, config)
-            config.truncate(0)
+def log_information(status):
+    """
+    Logs information to a log file.
+    """
+    separator = "=" * 30 + "\n"
+    with open("taskkill_logs.txt", "a", encoding="utf-8") as logs:
+        try:
+            for i in status:
+                logs.write(i)
+            logs.write("\n" + separator + "\n")
+        except Exception as e:
+            print("Unexpected Error Occured: {}".format(e))
+            exit(1)
 
-    def check_process(self):
-        """
-        Checks which processes are running and wether to end those processes or not.
-        """
-        data = [x.decode("windows-1252").split(",") for x in subprocess.check_output("TASKLIST /FO csv").splitlines()][3:]
-        proc = {x[0][1:-1]: int(x[1][1:-1]) for x in data}
-
-    def kill_process(self):
-        """
-        Calls the TASKKILL command to Windows CMD.
-        """
-        pass
-
-    def log_information(self, status):
-        """
-        Logs information to a log file.
-        """
-        separator = "=" * 30 + "\n"
-        with open("taskkill_logs.txt", "a", encoding="utf-8") as logs:
-            try:
-                logs.write("{} Kill Status: {}\n".format(separator, status))
-            except Exception as e:
-                print("Unexpected Error Occured: {}".format(e))
-                exit(1)
+if __name__ == "__main__":
+    check_process()
